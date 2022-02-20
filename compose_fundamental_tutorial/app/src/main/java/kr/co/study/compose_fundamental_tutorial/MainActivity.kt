063b168ec66f85ea6d879942d1f362eb33ddfe63
snack655 <80940200+snack655@users.noreply.github.com>
2022-02-20 20:56:19 +0900

    Study : Compose shape
    2022-02-20

--- compose_fundamental_tutorial/app/src/main/java/kr/co/study/compose_fundamental_tutorial/MainActivity.kt
+++ compose_fundamental_tutorial/app/src/main/java/kr/co/study/compose_fundamental_tutorial/MainActivity.kt
@@ -7,15 +7,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.center
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.*
@@ -26,9 +34,13 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kr.co.study.compose_fundamental_tutorial.ui.theme.Compose_fundamental_tutorialTheme
+import kotlin.math.cos
+import kotlin.math.sin
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
@@ -45,7 +57,8 @@ class MainActivity : ComponentActivity() {
                     //Container()
                     //VerticalContainer()
                     //BoxWithConstraintContainer()
-                    TextContainer()
+                    //TextContainer()
+                    ShapeContainer()
                 }
             }
         }
@@ -257,7 +270,6 @@ fun TextContainer() {
                         fontWeight = FontWeight.ExtraBold
                     )) {
                         append("$it ")
-                        Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
                     }
                 }else {
                     append("$it ")
@@ -274,15 +286,95 @@ fun TextContainer() {
     }
 }
 
+@Composable
+fun ShapeContainer() {
+
+    var polySides by remember { mutableStateOf(3) }
+
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        //DummyBox(modifier = Modifier.clip(RectangleShape))
+        DummyBox(modifier = Modifier.clip(CircleShape))
+        DummyBox(modifier = Modifier.clip(RoundedCornerShape(10.dp)))
+        DummyBox(modifier = Modifier.clip(CutCornerShape(10.dp)))
+        DummyBox(modifier = Modifier.clip(TriangleShape()))
+        DummyBox(modifier = Modifier.clip(PolyShape(polySides, 100f)))
+
+        Text(text = "ploySides: $polySides")
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(onClick = {
+                polySides += 1
+            }) {
+                Text(text = "plotSides + 1")
+            }
+            Button(onClick = {
+                polySides = 3
+            }) {
+                Text(text = "초기화")
+            }
+        }
+    }
+}
+
+class TriangleShape(): Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        val path = Path().apply {
+            moveTo(size.width / 2f, 0f)
+            lineTo(size.width, size.height)
+            lineTo(0f, size.height)
+            close()
+        }
+        return Outline.Generic(path = path)
+    }
+}
+
+class PolyShape(private val sides: Int, private val radius: Float) : Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        return Outline.Generic(path = Path().apply { polygon(sides, radius, size.center) })
+    }
+}
+
+fun Path.polygon(sides: Int, radius: Float, center: Offset) {
+    val angle = 2.0 * Math.PI / sides
+    moveTo(
+        x = center.x + (radius * cos(0.0)).toFloat(),
+        y = center.y + (radius * sin(0.0)).toFloat()
+    )
+    for (i in 1 until sides) {
+        lineTo(
+            x = center.x + (radius * cos(angle * i)).toFloat(),
+            y = center.y + (radius * sin(angle * i)).toFloat()
+        )
+    }
+    close()
+}
+
 @Preview(showBackground = true)
 @Composable
-fun MyPreView() {
+fun MyPreview() {
     Compose_fundamental_tutorialTheme {
         //Container()
         //Greeting("Android")
         //VerticalContainer()
         //BoxContainer()
         //BoxWithConstraintContainer()
-        TextContainer()
+        //TextContainer()
+        ShapeContainer()
     }
 }
