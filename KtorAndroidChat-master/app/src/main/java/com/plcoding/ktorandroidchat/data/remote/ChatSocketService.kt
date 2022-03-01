9d33a3fd5af470e2a0a4792e717ebe861408d3d8
snack655 <cmjcmj1230@gmail.com>
2022-03-01 09:02:57 +0900

    Add : ChatSocketService
    2022-03-01

---
+++ KtorAndroidChat-master/app/src/main/java/com/plcoding/ktorandroidchat/data/remote/ChatSocketService.kt
@@ -0,0 +1,27 @@
+package com.plcoding.ktorandroidchat.data.remote
+
+import com.plcoding.ktorandroidchat.domain.model.Message
+import com.plcoding.ktorandroidchat.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface ChatSocketService {
+
+    suspend fun initSession(
+        username: String
+    ): Resource<Unit>
+
+    suspend fun sendMessage(message: String)
+
+    fun observeMessages(): Flow<Message>
+
+    suspend fun closeSession()
+
+    companion object {
+        const val BASE_URL = "http://192.168.0.14:8082"
+    }
+
+    sealed class Endpoints(val url: String) {
+        object ChatSocket: Endpoints("$BASE_URL/chat-socket")
+    }
+
+}
